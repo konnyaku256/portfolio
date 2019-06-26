e2f7dfb08af1b5c2ca4b6a0138967a0b17331a5e
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2019-06-27 00:17:40 +0900

    :sparkles: Add link to Speaker Deck

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,8 @@
   <div class="footer">
     <a href='https://twitter.com/konnyaku256'><i class="fab fa-twitter"></i></a>
     <a href='https://github.com/konnyaku256'><i class="fab fa-github"></i></a>
-    <a href='https://medium.com/@konnyaku256'><i class="fab fa-medium"></i></i></a>
+    <a href='https://medium.com/@konnyaku256'><i class="fab fa-medium"></i></a>
+    <a href='https://speakerdeck.com/konnyaku256'><i class="fab fa-speaker-deck"></i></a>
   </div>
 </template>
 
