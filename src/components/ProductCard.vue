6ca048e1bf9a3e2ba89a731864269a0f9e0ed33e
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2023-01-29 15:35:33 +0900

    ProductCard のデザインを更新

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,33 +1,52 @@
 <template>
-  <div class="product-card w-full max-w-sm rounded overflow-hidden shadow-lg">
-    <a v-bind:href="product.link" class="block w-full h-full">
-      <picture>
-        <img v-bind:src="product.thumb" loading="lazy" class="w-full" />
-      </picture>
-      <section class="px-3 py-2 lg:px-6 lg:py-4">
-        <h3 class="font-bold text-lg lg:text-xl mb-2">
+  <div
+    class="product-card w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <a v-bind:href="product.link">
+      <img v-bind:src="product.thumb" loading="lazy" class="rounded-t-lg w-full" />
+    </a>
+    <div class="p-5">
+      <a v-bind:href="product.link">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {{ product.name }}
-        </h3>
-        <p class="text-sm lg:text-base">
-          {{ product.description }}
-        </p>
-      </section>
-      <ul class="p-3 lg:px-6 lg:py-4">
-        <li v-for="tag in product.tags" v-bind:key="tag" class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm lg:text-base font-semibold text-gray-700 mr-2 mb-2">
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {{ product.description }}
+      </p>
+      <ul class="pt-2 pb-2 mb-3">
+        <li
+          v-for="tag in product.tags"
+          v-bind:key="tag"
+          class="inline-block bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-300 text-sm font-medium rounded px-3 py-1 mr-2 mb-2"
+        >
           {{ tag }}
         </li>
       </ul>
-      <section class="px-3 py-2 lg:px-6 lg:py-4">
-        <p class="text-sm lg:text-base">
-          {{ product.date }}
-        </p>
-      </section>
-    </a>
+      <span
+        class="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-3 py-1 rounded mr-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500"
+      >
+        <svg
+          aria-hidden="true"
+          class="w-3 h-3 mr-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        {{ product.date }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType } from "vue";
 
 interface Product {
   name: string;
@@ -40,9 +59,9 @@ interface Product {
 export default defineComponent({
   props: {
     product: {
-      type: Object as PropType<Product>,
-    },
-  },
+      type: Object as PropType<Product>
+    }
+  }
 });
 </script>
 
