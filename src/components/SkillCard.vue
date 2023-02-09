278721c1ac7880983413cd49602d5b8fbd9b2335
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2023-02-09 14:37:11 +0900

    スキルの星のマージンを調整

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -2,7 +2,7 @@
   <div
     class="p-4 m-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700"
   >
-    <div class="flex items-center justify-between mb-4">
+    <div class="flex items-center justify-between mt-2 mb-2">
       <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
         <Star5 v-if="level === 5" />
         <Star4 v-else-if="level === 4" />
